b7b89bcee9f5a99647647e0e1851e0e4521b10be
Ardi Loot <ardi.loot@outlook.com>
2023-11-11 23:43:20 +0200

    format fixes

--- components/flow_meter/flow_meter.cpp
+++ components/flow_meter/flow_meter.cpp
@@ -1,19 +1,12 @@
 #include "flow_meter.h"
 
-
 namespace esphome {
 namespace flow_meter {
 
 static const char *const TAG = "flow_meter";
 
-Valve::Valve(const std::string& size, const std::vector<float>& s, const std::vector<float>& k) :
-  size_(size),
-  s_(s),
-  k_(k),
-  setting_(s.front())
-{
-
-}
+Valve::Valve(const std::string &size, const std::vector<float> &s, const std::vector<float> &k)
+    : size_(size), s_(s), k_(k), setting_(s.front()) {}
 
 bool Valve::next_valve_setting() {
   if (setting_ >= s_.back())
@@ -29,36 +22,26 @@ bool Valve::previous_valve_setting() {
   return true;
 }
 
-void ValveType::set_name(const std::string& name) {
-  name_ = name;
-}
+void ValveType::set_name(const std::string &name) { name_ = name; }
 
-void ValveType::add_size(const std::string& size, const std::vector<float>& s, const std::vector<float>& k) {
+void ValveType::add_size(const std::string &size, const std::vector<float> &s, const std::vector<float> &k) {
   sizes_.push_back(Valve(size, s, k));
 }
 
-void ValveType::next_valve_size() {
-  size_index_ = size_index_ + 1 >= sizes_.size() ? 0 : size_index_ + 1;
-}
+void ValveType::next_valve_size() { size_index_ = size_index_ + 1 >= sizes_.size() ? 0 : size_index_ + 1; }
 
-void ValveType::previous_valve_size() {
-  size_index_ = size_index_ <= 0 ? sizes_.size() - 1 : size_index_ - 1;
-}
+void ValveType::previous_valve_size() { size_index_ = size_index_ <= 0 ? sizes_.size() - 1 : size_index_ - 1; }
 
-void FlowMeter::loop() {
+void FlowMeter::loop() {}
 
-}
-
-float FlowMeter::get_setup_priority() const {
-  return setup_priority::LATE;
-}
+float FlowMeter::get_setup_priority() const { return setup_priority::LATE; }
 
 void FlowMeter::dump_config() {
   ESP_LOGCONFIG(TAG, "Flow component:");
 
   ESP_LOGCONFIG(TAG, "  Valves:");
-  for (const ValveType* valve_type : valve_types_) {
-    for (const Valve& valve : valve_type->get_sizes()) {
+  for (const ValveType *valve_type : valve_types_) {
+    for (const Valve &valve : valve_type->get_sizes()) {
       ESP_LOGCONFIG(TAG, "    %s-%s", valve_type->get_name().c_str(), valve.get_size().c_str());
     }
   }
@@ -69,10 +52,7 @@ void FlowMeter::dump_config() {
   LOG_SENSOR("  ", "Flow error", this->flow_error_sensor_);
 }
 
-void FlowMeter::add_valve_type(ValveType* valve_type) {
-  valve_types_.push_back(valve_type);
-}
-
+void FlowMeter::add_valve_type(ValveType *valve_type) { valve_types_.push_back(valve_type); }
 
 void FlowMeter::update(const float pressure) {
   ESP_LOGD(TAG, "update: %.1f Pa", pressure);
@@ -106,31 +86,27 @@ void FlowMeter::update(const float pressure) {
   if (this->flow_error_sensor_) {
     this->flow_error_sensor_->publish_state(flow_error);
   }
-
 }
 
 void FlowMeter::goto_next_state() {
-  switch (state_)
-  {
-  case State::IDLE:
-    pressure_stats_.clear();
-    state_ = State::AVERAGING;
-    break;
-  case State::AVERAGING:
-    state_ = State::STOPPED;
-    break;
-  case State::STOPPED:
-    pressure_stats_.clear();
-    state_ = State::IDLE;
-    break;
-  default:
-    break;
+  switch (state_) {
+    case State::IDLE:
+      pressure_stats_.clear();
+      state_ = State::AVERAGING;
+      break;
+    case State::AVERAGING:
+      state_ = State::STOPPED;
+      break;
+    case State::STOPPED:
+      pressure_stats_.clear();
+      state_ = State::IDLE;
+      break;
+    default:
+      break;
   }
 }
 
-const std::string& FlowMeter::active_valve_type_name() {
-  return active_valve_type().get_name();
-}
+const std::string &FlowMeter::active_valve_type_name() { return active_valve_type().get_name(); }
 
 void FlowMeter::next_valve_type() {
   valve_type_index_ = valve_type_index_ + 1 >= valve_types_.size() ? 0 : valve_type_index_ + 1;
@@ -140,29 +116,17 @@ void FlowMeter::previous_valve_type() {
   valve_type_index_ = valve_type_index_ <= 0 ? valve_types_.size() - 1 : valve_type_index_ - 1;
 }
 
-const std::string& FlowMeter::active_valve_size() const {
-  return active_valve().get_size();
-}
+const std::string &FlowMeter::active_valve_size() const { return active_valve().get_size(); }
 
-void FlowMeter::next_valve_size() {
-  active_valve_type().next_valve_size();
-}
+void FlowMeter::next_valve_size() { active_valve_type().next_valve_size(); }
 
-void FlowMeter::previous_valve_size() {
-  active_valve_type().previous_valve_size();
-}
+void FlowMeter::previous_valve_size() { active_valve_type().previous_valve_size(); }
 
-float FlowMeter::active_valve_setting() const {
-  return active_valve().get_setting();
-}
+float FlowMeter::active_valve_setting() const { return active_valve().get_setting(); }
 
-bool FlowMeter::next_valve_setting() {
-  return active_valve().next_valve_setting();
-}
+bool FlowMeter::next_valve_setting() { return active_valve().next_valve_setting(); }
 
-bool FlowMeter::previous_valve_setting() {
-  return active_valve().previous_valve_setting();
-}
+bool FlowMeter::previous_valve_setting() { return active_valve().previous_valve_setting(); }
 
 }  // namespace flow_meter
 }  // namespace esphome
--- components/flow_meter/flow_meter.h
+++ components/flow_meter/flow_meter.h
@@ -12,77 +12,70 @@
 namespace esphome {
 namespace flow_meter {
 
-enum struct State {
-  IDLE,
-  AVERAGING,
-  STOPPED
-};
+enum struct State { IDLE, AVERAGING, STOPPED };
 
 class Valve {
-  public:
-    Valve(const std::string& size, const std::vector<float>& s, const std::vector<float>& k);
-    bool next_valve_setting();
-    bool previous_valve_setting();
-    const std::string& get_size() const { return size_; }
-    float get_setting() const { return setting_; }
+ public:
+  Valve(const std::string &size, const std::vector<float> &s, const std::vector<float> &k);
+  bool next_valve_setting();
+  bool previous_valve_setting();
+  const std::string &get_size() const { return size_; }
+  float get_setting() const { return setting_; }
 
-    float interpolate_k() {
-      if (setting_ <= s_.front())
-        return k_.front();
+  float interpolate_k() {
+    if (setting_ <= s_.front())
+      return k_.front();
 
-      if (setting_ >= s_.back())
-        return k_.back();
-
-      // Seek for interval
-      for (int i = 1; i < s_.size(); i++) {
-        if (s_[i - 1] <= setting_ && s_[i] >= setting_) {
-          const float slope = (k_[i] - k_[i - 1]) / (s_[i] - s_[i - 1]);
-          const float interpolated_k = k_[i - 1] + slope * (setting_ - s_[i - 1]);
-          return interpolated_k;
-        }
-      }
+    if (setting_ >= s_.back())
       return k_.back();
-    }
 
-    float calc_flow(float dp){
-      
-      if (std::fabs(dp) < 0.1f)
-        return 0.0f; // Remove noise near zero pressure
-
-      const float kv = interpolate_k();
-      if (dp < 0.0f){
-        return -kv * std::sqrt(-dp);
-      } else {
-        return kv * std::sqrt(dp);
+    // Seek for interval
+    for (int i = 1; i < s_.size(); i++) {
+      if (s_[i - 1] <= setting_ && s_[i] >= setting_) {
+        const float slope = (k_[i] - k_[i - 1]) / (s_[i] - s_[i - 1]);
+        const float interpolated_k = k_[i - 1] + slope * (setting_ - s_[i - 1]);
+        return interpolated_k;
       }
     }
-    
-  protected:
-    std::string size_;
-    std::vector<float> s_;
-    std::vector<float> k_;
-    float setting_ {0.0f};
-};
+    return k_.back();
+  }
 
+  float calc_flow(float dp) {
+    if (std::fabs(dp) < 0.1f)
+      return 0.0f;  // Remove noise near zero pressure
+
+    const float kv = interpolate_k();
+    if (dp < 0.0f) {
+      return -kv * std::sqrt(-dp);
+    } else {
+      return kv * std::sqrt(dp);
+    }
+  }
+
+ protected:
+  std::string size_;
+  std::vector<float> s_;
+  std::vector<float> k_;
+  float setting_{0.0f};
+};
 
 class ValveType {
-public:
-  void set_name(const std::string& name);
-  void add_size(const std::string& size, const std::vector<float>& s, const std::vector<float>& k);
+ public:
+  void set_name(const std::string &name);
+  void add_size(const std::string &size, const std::vector<float> &s, const std::vector<float> &k);
   void next_valve_size();
   void previous_valve_size();
-  const std::string& get_name() const { return name_; }
-  const std::vector<Valve>& get_sizes() const { return sizes_; }
-  std::vector<Valve>& get_sizes() { return sizes_; }
-  Valve& active_valve() { return sizes_.at(size_index_); }
-  const Valve& active_valve() const { return sizes_.at(size_index_); }
+  const std::string &get_name() const { return name_; }
+  const std::vector<Valve> &get_sizes() const { return sizes_; }
+  std::vector<Valve> &get_sizes() { return sizes_; }
+  Valve &active_valve() { return sizes_.at(size_index_); }
+  const Valve &active_valve() const { return sizes_.at(size_index_); }
 
-protected:
+ protected:
   std::string name_;
   std::vector<Valve> sizes_;
-  int size_index_ {0};
+  int size_index_{0};
 };
-
 
 class FlowMeter : public Component {
  public:
@@ -90,23 +83,23 @@ class FlowMeter : public Component {
   float get_setup_priority() const override;
   void dump_config() override;
 
-  void add_valve_type(ValveType* valve_type);
-  void set_flow_sensor(sensor::Sensor* sensor) { flow_sensor_ = sensor; }
-  void set_pressure_avg_sensor(sensor::Sensor* sensor) { pressure_avg_sensor_ = sensor; }
-  void set_pressure_error_sensor(sensor::Sensor* sensor) { pressure_error_sensor_ = sensor; }
-  void set_flow_avg_sensor(sensor::Sensor* sensor) { flow_avg_sensor_ = sensor; }
-  void set_flow_error_sensor(sensor::Sensor* sensor) { flow_error_sensor_ = sensor; }
+  void add_valve_type(ValveType *valve_type);
+  void set_flow_sensor(sensor::Sensor *sensor) { flow_sensor_ = sensor; }
+  void set_pressure_avg_sensor(sensor::Sensor *sensor) { pressure_avg_sensor_ = sensor; }
+  void set_pressure_error_sensor(sensor::Sensor *sensor) { pressure_error_sensor_ = sensor; }
+  void set_flow_avg_sensor(sensor::Sensor *sensor) { flow_avg_sensor_ = sensor; }
+  void set_flow_error_sensor(sensor::Sensor *sensor) { flow_error_sensor_ = sensor; }
 
   void update(const float pressure);
 
   void goto_next_state();
   State get_state() const { return state_; }
-  
-  const std::string& active_valve_type_name();
+
+  const std::string &active_valve_type_name();
   void next_valve_type();
   void previous_valve_type();
-  
-  const std::string& active_valve_size() const;
+
+  const std::string &active_valve_size() const;
   void next_valve_size();
   void previous_valve_size();
 
@@ -117,24 +110,23 @@ class FlowMeter : public Component {
   uint32_t get_sample_count() const { return pressure_stats_.count(); }
 
  protected:
-  std::vector<ValveType*> valve_types_;
-  int valve_type_index_ {0};
+  std::vector<ValveType *> valve_types_;
+  int valve_type_index_{0};
 
-  State state_ {State::IDLE};
+  State state_{State::IDLE};
   statistic::Statistic<float, uint32_t, true> pressure_stats_;
 
-  sensor::Sensor* flow_sensor_ {nullptr};
-  sensor::Sensor* pressure_avg_sensor_ {nullptr};
-  sensor::Sensor* pressure_error_sensor_ {nullptr};
-  sensor::Sensor* flow_avg_sensor_ {nullptr};
-  sensor::Sensor* flow_error_sensor_ {nullptr};
+  sensor::Sensor *flow_sensor_{nullptr};
+  sensor::Sensor *pressure_avg_sensor_{nullptr};
+  sensor::Sensor *pressure_error_sensor_{nullptr};
+  sensor::Sensor *flow_avg_sensor_{nullptr};
+  sensor::Sensor *flow_error_sensor_{nullptr};
 
-  ValveType& active_valve_type() { return *valve_types_.at(valve_type_index_); }
-  const ValveType& active_valve_type() const { return *valve_types_.at(valve_type_index_); }
-  Valve& active_valve() { return active_valve_type().active_valve(); }
-  const Valve& active_valve() const { return active_valve_type().active_valve(); }
-
+  ValveType &active_valve_type() { return *valve_types_.at(valve_type_index_); }
+  const ValveType &active_valve_type() const { return *valve_types_.at(valve_type_index_); }
+  Valve &active_valve() { return active_valve_type().active_valve(); }
+  const Valve &active_valve() const { return active_valve_type().active_valve(); }
 };
 
-}  // namespace debug
+}  // namespace flow_meter
 }  // namespace esphome
